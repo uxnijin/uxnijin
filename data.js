47d10012675103d470b9c0011bef06044f95ec13
Nijin <nijuvenghat550649@gmail.com>
2025-03-07 09:03:36 +0530

    more changes

---
+++ data.js
@@ -0,0 +1,47 @@
+const workExperience = [
+    {
+        title: "Product Designer at Keviz R&D Studio",
+        date: "Dec 2024 - Present",
+        image: "experience1.jpg",
+        live: true,
+        link: "https://www.linkedin.com/company/keviz/posts/?feedView=all" // Example link
+    },
+    {
+        title: "UI UX Designer at Qmark Technolabs",
+        date: "Apr 2024 - Sep 2024",
+        image: "experience2.jpg",
+        live: false,
+        link: "https://www.qmarktechnolabs.com" // Example link
+    },
+    {
+        title: "Design Intern at Tinkerhub Organization",
+        date: "Sep 2023 - Jun 2024",
+        image: "experience3.jpg",
+        live: false,
+        link: "https://tinkerhub.org/" // Example link
+    },
+    {
+        title: "UI UX Designer at RITS",
+        date: "Sep 2023 - Jun 2024",
+        image: "experience4.jpg",
+        live: false,
+        link: "https://www.rits.ae/#/home" // Example link
+    }
+];
+
+const education = [
+    {
+        title: "BSc Computer Science at Farook College",
+        date: "2021-2024",
+        image: "education1.jpg",
+        live: false,
+        link: "https://farookcollege.ac.in/" // Example link
+    },
+    {
+        title: "Farook Higher Secondary School",
+        date: "Sep 2023 - Jun 2024",
+        image: "education2.jpg",
+        live: false,
+        link: "https://www.tinkerhub.org" // Example link
+    }
+];
