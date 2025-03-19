3018d2f61bbdc892685b7679a3c70542375af6f2
Nijin <nijuvenghat550649@gmail.com>
2025-03-19 10:28:42 +0530

    mycreations added and changed the mydesigns page to js dynamic and removed dribbble

--- data.js
+++ data.js
@@ -4,28 +4,28 @@ const workExperience = [
         date: "Dec 2024 - Present",
         image: "experience1.png",
         live: true,
-        link: "https://www.linkedin.com/company/keviz/posts/?feedView=all" // Example link
+        link: "https://www.linkedin.com/company/keviz/posts/?feedView=all"
     },
     {
         title: "UI UX Designer at Qmark Technolabs",
         date: "Apr 2024 - Sep 2024",
-        image: "experience2.png ",
+        image: "experience2.png",
         live: false,
-        link: "https://www.qmarktechnolabs.com" // Example link
+        link: "https://www.qmarktechnolabs.com"
     },
     {
         title: "Design Intern at Tinkerhub Organization",
         date: "Sep 2023 - Jun 2024",
         image: "experience3.png",
         live: false,
-        link: "https://tinkerhub.org/" // Example link
+        link: "https://tinkerhub.org/"
     },
     {
         title: "UI UX Designer at RITS",
         date: "Sep 2023 - Jun 2024",
         image: "experience4.png",
         live: false,
-        link: "https://www.rits.ae/#/home" // Example link
+        link: "https://www.rits.ae/#/home"
     }
 ];
 
@@ -35,13 +35,39 @@ const education = [
         date: "2021-2024",
         image: "education1.png",
         live: false,
-        link: "https://farookcollege.ac.in/" // Example link
+        link: "https://farookcollege.ac.in/"
     },
     {
         title: "Farook Higher Secondary School",
         date: "Sep 2023 - Jun 2024",
         image: "education1.png",
         live: false,
-        link: "https://farookcollege.ac.in/sister-institutions" // Example link
+        link: "https://farookcollege.ac.in/sister-institutions"
+    }
+];
+
+const designWorks = [
+    {
+        title: "Zaiqa Restaurant",
+        description: "An Ireland-based restaurant with food ordering, table booking and detailed restaurant information",
+        date: "Feb 2025",
+        thumbnail: "assets/design/mybilling.png",
+        designLink: "https://www.figma.com/design/tyFdQXNPgW3ifAUqkEvxu5/Zaiqa-Restaurant?node-id=0-1&t=hSyVxWlYzkbCFIWG-1",
+        liveLink: "https://mybilling-web.vercel.app/"
+    },
+    {
+        title: "Hany Bakery",
+        description: "Saudi Arabia-based Bakery, featuring online orders and a showcase of their specialty baked goods.",
+        date: "Jan 2025",
+        thumbnail: "assets/design/hanybakery.png",
+        designLink: "https://www.figma.com/design/1VPadipmCliFS38gMY8EV7/Hany-Bakery?node-id=0-1&t=rHNPpi5hcm65ncaN-1",
+        liveLink: "https://www.hanybakery.com/"
+    }
+];
+const creations = [
+    {
+        title: "Aazan",
+        icon: "assets/creations/aazan.png",
+        link: "https://aazan.org"
     }
 ];
